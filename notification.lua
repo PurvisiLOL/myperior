87a8c1a1ea8333ce6d6b2ac8a18ae80db02facb5
PurvisiLOL <157009854+PurvisiLOL@users.noreply.github.com>
2024-10-09 16:20:13 +0200

    Create notification.lua

---
+++ notification.lua
@@ -0,0 +1,8 @@
+local NotificationHolder = loadstring(game:HttpGet("https://pastebin.com/raw/t3Kr3Gve"))()
+local Notification = loadstring(game:HttpGet("https://pastebin.com/raw/YJ8wM6PM"))()
+
+Notification:Notify(
+        {Title = "Myperior", Description = "Myperior has been injected!"},
+        {OutlineColor = Color3.fromRGB(18, 18, 18),Time = 4, Type = "image"},
+		{Image = "rbxassetid://124961528312023", ImageColor = Color3.fromRGB(255, 255, 255)}
+)
